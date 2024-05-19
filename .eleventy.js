f4596d72ba315fcc05a88fd5934fca359b7a5586
Abdullah Yahya <ayahya@qualys.com>
2024-05-19 11:03:24 -0700

    updated config

--- .eleventy.js
+++ .eleventy.js
@@ -1,24 +1,24 @@
 module.exports = function(eleventyConfig) {
-	// eleventyConfig.addPassthroughCopy("src", {
-	// 	//debug: true,
-	// 	filter: [
-	// 		"404.html",
-	// 		"**/*.css",
-	// 		"**/*.js",
-	// 		"**/*.json",
-	// 		"!**/*.11ty.js",
-	// 		"!**/*.11tydata.js",
-	// 	]
-	// });
-  // 
-	// // Copy img folder
-	// eleventyConfig.addPassthroughCopy("src/img");
-// 
-	// eleventyConfig.setServerPassthroughCopyBehavior("copy");
+	eleventyConfig.addPassthroughCopy("src", {
+		//debug: true,
+		filter: [
+			"404.html",
+			"**/*.css",
+			"**/*.js",
+			"**/*.json",
+			"!**/*.11ty.js",
+			"!**/*.11tydata.js",
+		]
+	});
+  
+	// Copy img folder
+	eleventyConfig.addPassthroughCopy("src/img");
 
-	eleventyConfig.ignores.add("**/*.css");
-	eleventyConfig.ignores.add("**/*.js");
-	eleventyConfig.ignores.add("**/*.json");
+	eleventyConfig.setServerPassthroughCopyBehavior("copy");
+
+	// eleventyConfig.ignores.add("**/*.css");
+	// eleventyConfig.ignores.add("**/*.js");
+	// eleventyConfig.ignores.add("**/*.json");
 
 	return {
 		dir: {
