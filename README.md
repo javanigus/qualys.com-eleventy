0399029e02c369f283ea06e3735918c2f4f6e4b9
Abdullah Yahya <ayahya@qualys.com>
2024-05-20 10:27:24 -0700

    Added more config files and utilities

---
+++ README.md
@@ -0,0 +1,3 @@
+# www.qualys.com
+
+A migration of the Qualys website to Eleventy, Github and Netlify. This approach migrates the static HTML pages rather than converts all Handlebars references to Nunjucks references. The header and footer are moved to partials. 
---
+++ utils/contentful-technology-partners.js
@@ -0,0 +1,41 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const jmespath = require('jmespath');
+require('dotenv').config({ path: require('find-config')('.env') })
+
+// Contentful API docs
+// https://www.contentful.com/developers/docs/references/content-delivery-api/
+const space_id = "4l0w8syj29ap";
+const environment_id = "master";
+const access_token = process.env.CONTENTFUL_ACCESS_TOKEN;
+const content_type = "technologyPartner";
+const limit = "1000"; // max API query results
+const url = `https://cdn.contentful.com//spaces/${space_id}/environments/${environment_id}/entries?access_token=${access_token}&limit=${limit}&content_type=${content_type}`;
+
+console.log(url);
+
+module.exports = async function() {
+    /* This returns a promise */
+    return EleventyFetch(url, {
+	    duration: "1d", // save for 1 day
+	    type: "json"    // we’ll parse JSON for you
+    });
+
+	// Use jmespath to filter JSON data
+    // https://www.npmjs.com/package/jmespath?activeTab=readme
+	var filteredData = jmespath.search(data, "items[0].fields");
+
+	console.log(filteredData);
+
+	return filteredData;
+
+	// simplify data
+	var results = data.items.map(function (currentValue, index, arr) {
+		return currentValue.fields;
+	});
+
+	var simpleData = {items: results};
+	
+	console.log(simpleData);
+
+	return simpleData;
+}; 
